04819a09d5bd73954eb85717c84845b36fab42a0
Vishwas Srivastava <vishwassrivastava1991@gmail.com>
2024-03-04 13:37:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ import Translate from "translate";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.get("/", (req, res) => {
   res.send("heelppp");
 });
