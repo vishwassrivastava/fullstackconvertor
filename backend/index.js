4a0c5362419d273373e4cb2b250814dbacf0a6e0
Vishwas Srivastava <vishwassrivastava1991@gmail.com>
2024-03-04 14:01:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,11 @@ import Translate from "translate";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions))
+
+app.use(cors({
+  origin: 'https://fullstackconvertor.vercel.app' // Allow your frontend origin
+}));
 app.get("/", (req, res) => {
   res.send("heelppp");
 });
